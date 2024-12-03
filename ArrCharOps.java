31a21359a53cb5904255b4bd43f9ab8a5a39d883
noa-halevi <noahalv123@gmail.com>
2024-12-03 21:42:38 +0200

    Update ArrCharOps.java

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -170,16 +170,17 @@ public class ArrCharOps {
             char ch2 = str2.charAt(i);
             if(ch1 < ch2){
                 return -1;
-            } else if(ch1 > ch2){
+            } 
+            else if (ch1 > ch2){
                 return 1;
             }
         }
-    if (len1 < len2) {
-        return -1;  
-    } else if (len1 > len2) {
-        return 1;  
-    }     
-    return 0;
+        if (len1 < len2) {
+            return -1;  
+        } else if (len1 > len2) {
+            return 1;  
+        } 
+        return 0;
 }
 
 }
