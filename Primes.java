88b5a2ae6900b67efd4e3611fc43f7e6267abd23
noa-halevi <noahalv123@gmail.com>
2024-12-03 21:59:48 +0200

    Update Primes.java

--- Primes.java
+++ Primes.java
@@ -2,7 +2,7 @@ public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         boolean[] isPrime = new boolean[n+1];
-        for(int i=2; i<n+1; i++){
+        for(int i=2; i<=n; i++){
             isPrime[i]=true;          
         }
         for(int p = 2; p<Math.sqrt(n); p++){
@@ -14,14 +14,14 @@ public class Primes {
         }
         System.out.println("Prime numbers up to " + n + ":");
         int count=0;
-        for(int i=2; i<n; i++){
+        for(int i=2; i<=n; i++){
             if(isPrime[i] == true){
                 System.out.println(i);
                 count++;
             } 
         }
         double percentage = (count*100)/n;
-        System.out.println("There are " + 10 + " primes between 2 and " + n + " (" + (int) percentage + "% are primes)");
+        System.out.println("There are " + count + " primes between 2 and " + n + " (" + (int) percentage + "% are primes)");
        
           } 
     }
